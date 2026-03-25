d7e9a40aa688ad10237aea93ce829eef9ebcf548
erpalmon <ellapalmon@gmail.com>
2026-03-25 11:25:38 -0400

    CSS Additions/Modification, View folder files modified

--- src/View/Home/Home.js
+++ src/View/Home/Home.js
@@ -5,6 +5,8 @@ function Home() {
         <div className="home-container">
             <div className="navbar">
                 <h2 className="logo">Foodblog</h2>
+                <h2 className="nav-item">Upload</h2>
+                <h2 className="nav-item2">About Us</h2>
             </div>
         </div>
     )
